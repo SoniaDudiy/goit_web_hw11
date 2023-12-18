b16ab6c1082bae091929373305c45af6cf556fe6
Sonia <sonia09012004@gmail.com>
2023-12-18 11:45:09 +0200

    first

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ app.include_router(contacts.router)
 app.include_router(search.search)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=9000, reload=True)
